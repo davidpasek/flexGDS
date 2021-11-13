ddb60d11eb1b36ba45ece69e0d4aba902bb39b89
davidpasek <david.pasek@gmail.com>
2021-11-13 23:56:19 +0000

    PHP scripts to show FlexBook Nodes identification.

--- apache/index.php
+++ apache/index.php
@@ -1,3 +1,11 @@
-<?php
-phpinfo();
-?>
+<html>
+  <head>
+    <title>FlexBook Node</title>
+  </head>
+<body>
+<p>FlexBook Provider FQDN: <b><?php echo $_SERVER['SERVER_NAME']; ?></b></p>
+<p>FlexBook Node IP address: <b><?php echo $_SERVER['SERVER_ADDR']; ?></b></p>
+<hr> <a href="info.php">PHPINFO</a></hr>
+</body>
+</html>
+
